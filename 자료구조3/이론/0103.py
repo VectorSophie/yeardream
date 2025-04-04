2e2ea9fd5b56929305915575e6db7f0a6eccf6e7
VectorSophie <jay7math@gmail.com>
2025-04-04 10:34:48 +0900

    uhh

---
+++ 자료구조3/이론/0103.py
@@ -0,0 +1,22 @@
+import heapq
+
+class PriorityQueue:
+   
+    def __init__(self) :
+        self.data = []
+
+    def push(self, value) :
+       
+        heapq.heappush(self.data, (abs(value), value))
+
+    def pop(self) :
+        
+        if len(self.data) > 0:
+            heapq.heappop(self.data)
+
+    def top(self) :
+        
+        if len(self.data) == 0:
+            return -1
+        else:
+            return self.data[0][1]
